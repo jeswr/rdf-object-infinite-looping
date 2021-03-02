66ed283883f487e5c67ef5c310eb9d81b6854fc4
jeswr <jesse.wright@anu.edu.au>
2021-03-03 01:00:21 +1100

    adding minimal example

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,12 @@
-export default {};
+import { RdfObjectLoader } from 'rdf-object';
+import { namedNode, quad } from '@rdfjs/data-model';
+
+(async () => {
+  const loader = new RdfObjectLoader();
+  await loader.importArray([
+    quad(namedNode('thing'), namedNode('a'), namedNode('thing'))
+  ]);
+  const thing = loader.resources['thing'];
+
+  console.log(thing.toQuads());
+})
