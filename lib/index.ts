a7a55b915265f1274e5bd1c8ccf4480d782bc474
jeswr <jesse.wright@anu.edu.au>
2021-03-03 01:05:33 +1100

    adding another error case

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import { RdfObjectLoader } from 'rdf-object';
 import { namedNode, quad } from '@rdfjs/data-model';
 
+// Self referential
 (async () => {
   const loader = new RdfObjectLoader();
   await loader.importArray([
@@ -9,4 +10,16 @@ import { namedNode, quad } from '@rdfjs/data-model';
   const thing = loader.resources['thing'];
 
   console.log(thing.toQuads());
-})
+})();
+
+// Circular reference
+(async () => {
+  const loader = new RdfObjectLoader();
+  await loader.importArray([
+    quad(namedNode('jesse'), namedNode('knows'), namedNode('bob')),
+    quad(namedNode('bob'), namedNode('knows'), namedNode('jesse')),
+  ]);
+  const thing = loader.resources['thing'];
+
+  console.log(thing.toQuads());
+})();
